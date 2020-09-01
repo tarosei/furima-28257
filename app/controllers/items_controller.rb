d1a2b2bebf5de2231a2bd8a6dfaf7081d87cc8f1
tarosei <66665784+tarosei@users.noreply.github.com>
2020-09-01 21:29:38 +0900

    コンフリクト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index, except: [:index, :show]
+ 
   def index
+  end
+
+  def edit
+    
+  end
+
+  def new
+    
+  end
+
+  def show
+    
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
