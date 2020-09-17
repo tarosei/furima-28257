9a433062f7443a675649934714d795267091c142
tarosei <66665784+tarosei@users.noreply.github.com>
2020-09-17 20:01:17 +0900

    エラーの解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:  'items#index'
-  resources :items, only:[:edit, :update, :show, :new, :create, :destroy]  
-  # destoryアクションまでのルーティングを作る
-  # Controllerでdestroyアクションを定義する
-  # ルーティングを見ながら、link_to(削除ボタン)にdestroyアクションを動かすためのpathを設定してあげる
+  resources :items, only:[:edit, :update, :show, :new, :create, :destroy]
 end
