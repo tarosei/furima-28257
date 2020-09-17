9c478ace8b228c607fc68461df643f69818929b8
tarosei <66665784+tarosei@users.noreply.github.com>
2020-09-17 19:22:01 +0900

    アイテム削除する機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:  'items#index'
-  resources :items, only:[:edit, :update, :show, :new, :create]  
-  
+  resources :items, only:[:edit, :update, :show, :new, :create, :destroy]  
+  # destoryアクションまでのルーティングを作る
+  # Controllerでdestroyアクションを定義する
+  # ルーティングを見ながら、link_to(削除ボタン)にdestroyアクションを動かすためのpathを設定してあげる
 end
